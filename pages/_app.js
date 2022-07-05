968a55e02bc53b02a3a989251a37b2b608be304b
Jarrod Watts <jarrodwatts16@gmail.com>
2022-07-05 12:39:04 +1000

    Add metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import Head from "next/head";
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
@@ -7,6 +8,18 @@ const activeChainId = ChainId.Mumbai;
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+      <Head>
+        <title>thirdweb NFT Collection</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Learn How To Use Thirdweb's NFT Collection contract and create a page to show your collection"
+        />
+        <meta
+          name="keywords"
+          content="Thirdweb, thirdweb NFT collection, how to make nft collection thirdweb"
+        />
+      </Head>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
